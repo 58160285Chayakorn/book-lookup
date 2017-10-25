76d9aa615fd67ecdc416c223f43e76b57a8d18e5
Chayakorn Chayaphaniphatt <58160285@go.buu.ac.th>
2017-10-25 12:11:10 +0700

    PassTest: MockAmazonSer have been called and called with

---
+++ book-lookup.spec.js
@@ -0,0 +1,38 @@
+function bookLookUp(AmazonService){
+  this.searchService = AmazonService
+  this.search = (ispn) => {
+    var bookDetail = AmazonService(ispn)
+    return {
+      bookName: bookDetail.bookName,
+      cover: bookDetail.cover,
+      ispn: "0000000000000"
+
+    }
+  }
+
+}
+
+
+test('Book look up', () => {
+  //Arrange
+  const MockAmazonService = jest.fn().mockReturnValue({
+    bookName: 'Harry Potter',
+    cover: 'bookCover.jpg',
+    ispn: '1001234567890'
+
+
+  })
+  var app = new bookLookUp(MockAmazonService)
+
+  //Act
+  var ispn = "1001234567890"
+  var result = app.search(ispn)
+  //Assert
+  expect(MockAmazonService).toHaveBeenCalled()
+  expect(MockAmazonService).toHaveBeenCalledWith(ispn)
+
+
+
+
+
+})
